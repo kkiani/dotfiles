3968d597987978e9f13d7e7760f3dc32edd4ddcd
kiarash kiani <kiarash@kiani.info>
2023-01-09 10:33:52 +0100

    feat(statusbar): add emoji to the space status

--- swiftbar/currentspace.10m.sh
+++ swiftbar/currentspace.10m.sh
@@ -1,3 +1,3 @@
 current_space=$(yabai -m query --spaces --space | jq ".index")
 number_of_spaces=$(yabai -m query --spaces | jq length)
-echo "$current_space / $number_of_spaces"
+echo ":macwindow.on.rectangle: $current_space / $number_of_spaces"
