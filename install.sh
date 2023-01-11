bd6dcded982c604657c141bc8a549465717af3c6
kiarash kiani <kiarash@kiani.info>
2023-01-11 10:49:07 +0100

    refactor(installation): clean-up commented commands

--- install.sh
+++ install.sh
@@ -8,9 +8,6 @@ function warning() {
     echo -e "\e[1m\e[33mWarning:\e[39m $@\e[0m" 
 }
 
-# Uncomment the following line to delete all symlinks at the root of $HOME - useful for reinstalls
-# find "$HOME" -maxdepth 1 -type l -exec rm -f {} \;
-
 SELF_PATH="$( cd "$( dirname "$0" )" && pwd )" # Path to the directory containing this script
 
 # Create symlinks
@@ -46,12 +43,5 @@ ln -sv "$HOME/Library/Mobile Documents/com~apple~CloudDocs/Backups/espanso" "$HO
 ln -sv "$HOME/Library/Mobile Documents/com~apple~CloudDocs/Backups/.aws" "$HOME/.aws"
 
 # installing SpaceVim
+heading "[vim] installing SpaceVim..."
 curl -sLf https://spacevim.org/install.sh | bash
-
-# vim -c ':call coc#util#install()'
-
-# echo "Updating termcap database..."
-# $SELF_PATH/term/term.sh
-
-# Needed on macOS Catalina. Source: https://stackoverflow.com/a/22753363
-# compaudit | xargs chmod g-w
