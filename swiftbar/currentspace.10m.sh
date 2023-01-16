af6fc17f75c2c77cb478537735cad2a67efee0df
kiarash kiani <kiarash@kiani.info>
2023-01-16 18:49:49 +0100

    chore: remove swiftbar files

--- swiftbar/currentspace.10m.sh
+++
@@ -1,3 +0,0 @@
-current_space=$(yabai -m query --spaces --space | jq ".index")
-number_of_spaces=$(yabai -m query --spaces | jq length)
-echo ":macwindow.on.rectangle: $current_space / $number_of_spaces"
