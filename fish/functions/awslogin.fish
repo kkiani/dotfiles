ee2a27142743d4170c35f51a2fc9af5ba32af53e
Kiarash Kiani <kiarash.kiani@icloud.com>
2023-07-29 14:47:54 +0200

    fix(fish): update awslogin fish function

--- fish/functions/awslogin.fish
+++ fish/functions/awslogin.fish
@@ -1,4 +1,4 @@
-function awslogin "Login to AWS using aws-vault"
+function awslogin --description "Login to AWS using aws-vault"
     set -l profile $(aws configure list-profiles | fzf)
     command aws-vault exec "$profile" --prompt=osascript --server
 end
