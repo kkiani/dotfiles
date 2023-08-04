db5d70b14c39628990ee9357970fb047df3f4b50
Kiarash Kiani <kiarash.kiani@icloud.com>
2023-08-04 15:41:39 +0200

    fix(nvim): telescope not showing file names starts with git

--- nvim/plugin/telescope.lua
+++ nvim/plugin/telescope.lua
@@ -1,18 +1,17 @@
-require('telescope').setup({
-    defaults = {
-        layout_config = {
-            horizontal = {
-                width = 0.9,
-                height = 0.9,
-                preview_width = 0.6
-            }
-            -- other layout configuration here
-        },
-        file_ignore_patterns = {
-            "node_modules",
-            ".venv",
-            ".git",
-            ".vscode"
-        }
-    },
+require("telescope").setup({
+	defaults = {
+		layout_config = {
+			horizontal = {
+				width = 0.9,
+				height = 0.9,
+				preview_width = 0.6,
+			},
+			-- other layout configuration here
+		},
+		file_ignore_patterns = {
+			"node_modules",
+			".venv",
+			".vscode",
+		},
+	},
 })
