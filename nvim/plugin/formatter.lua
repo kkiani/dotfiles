94d943ec32b00bb7cab376a952c0072d7928f69d
Kiarash Kiani <kiarash.kiani@icloud.com>
2023-08-04 22:18:37 +0200

    feat(nvim): add support for formatter

--- nvim/plugin/formatter.lua
+++ nvim/plugin/formatter.lua
@@ -1,6 +1,16 @@
 -- Utilities for creating configurations
 local util = require("formatter.util")
 
+local function prettier()
+	return {
+		exe = "prettier",
+		args = {
+			util.escape_path(util.get_current_buffer_file_path()),
+		},
+		stdin = true,
+	}
+end
+
 -- Provides the Format, FormatWrite, FormatLock, and FormatWriteLock commands
 require("formatter").setup({
 	-- Enable or disable logging
@@ -13,12 +23,14 @@ require("formatter").setup({
 		-- and will be executed in order
 		lua = { require("formatter.filetypes.lua").stylua },
 		python = { require("formatter.filetypes.python").black },
+		typescript = { prettier },
+		javascript = { prettier },
+		json = { prettier },
+		yaml = { prettier },
 
 		-- Use the special "*" filetype for defining formatter configurations on
 		-- any filetype
 		["*"] = {
-			-- "formatter.filetypes.any" defines default configurations for any
-			-- filetype
 			require("formatter.filetypes.any").remove_trailing_whitespace,
 		},
 	},
