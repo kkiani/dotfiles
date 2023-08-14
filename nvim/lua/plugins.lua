2423c9b0ab44efb91ff4c93a02d5a9e0f6f7e923
Kiarash Kiani <kiarash.kiani@icloud.com>
2023-08-14 12:08:30 +0200

    feat(nvim): add plugin to show parses of treesitter

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -14,6 +14,7 @@ return require("packer").startup(function(use)
 		requires = { "nvim-tree/nvim-web-devicons", opt = true },
 	})
 	use({ "nvim-treesitter/nvim-treesitter", run = ":TSUpdate" })
+	use({ "nvim-treesitter/playground" })
 	use({
 		"nvim-telescope/telescope.nvim",
 		tag = "0.1.2",
