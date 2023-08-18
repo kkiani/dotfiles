4c0b1acf0b22a04e27923ea80f54169113ec400b
Kiarash Kiani <kiarash.kiani@icloud.com>
2023-08-18 13:41:55 +0200

    fix(nvim): revert tasks palette to use telescope again

--- nvim/lua/tasks.lua
+++ nvim/lua/tasks.lua
@@ -5,8 +5,11 @@ local tasks = vim.split(vim.fn.glob(tasks_directory .. "/*.sh"), "\n", { trimemp
 
 vim.api.nvim_create_user_command("TasksRun", function()
 	if next(tasks) ~= nil then
-		vim.ui.select(tasks, { prompt = " Choose a bash script task to run" }, function(item, idx)
-			Utils.exec({ ":term bash", item })
+		Utils.picker({
+			title = "Choose a bash script task to run",
+			values = tasks,
+		}, function(selection)
+			Utils.exec({ ":term bash", selection[1] })
 		end)
 	else
 		print("No tasks defined for this project")
