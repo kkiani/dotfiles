96a1408f8399e138a8214e62503c5576e7b8f708
Kiarash Kiani <kiarash.kiani@icloud.com>
2023-08-22 16:43:17 +0200

    feat(nvim): update lualine

--- nvim/plugin/lualine.lua
+++ nvim/plugin/lualine.lua
@@ -22,7 +22,7 @@ require("lualine").setup({
 		},
 		ignore_focus = {},
 		always_divide_middle = true,
-		globalstatus = false,
+		globalstatus = true,
 		refresh = {
 			statusline = 1000,
 			tabline = 1000,
@@ -48,28 +48,25 @@ require("lualine").setup({
 	tabline = {
 		lualine_a = {
 			{
-				"tabs",
-				mode = 1,
-				fmt = function(name, context)
-					-- Show + if buffer is modified in tab
-					local buflist = vim.fn.tabpagebuflist(context.tabnr)
-					local winnr = vim.fn.tabpagewinnr(context.tabnr)
-					local bufnr = buflist[winnr]
-					local mod = vim.fn.getbufvar(bufnr, "&mod")
-
-					return name .. (mod == 1 and " [*]" or "")
-				end,
+				"filename",
+				path = 1,
+				icon = { "󰈙", align = "left" },
+				separator = {},
+				symbols = {
+					readonly = "",
+				},
 			},
 		},
 		lualine_b = {},
 		lualine_c = {},
 		lualine_x = {},
-		lualine_y = { get_curret_buffer_path },
+		lualine_y = {},
 		lualine_z = {
 			{
 				"close",
+				separator = {},
 				fmt = function()
-					return "󰅖"
+					return ""
 				end,
 				on_click = close_current_buffer,
 			},
