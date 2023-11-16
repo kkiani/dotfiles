9910767ab37cd5d33d6c35a4e28eb457a018e834
Kiarash Kiani <kiarash.kiani@icloud.com>
2023-11-16 13:12:01 +0100

    feat(nvim): replacing netrw with oil

---
+++ nvim/plugin/oil.lua
@@ -0,0 +1,11 @@
+require("oil").setup({
+	skip_confirm_for_simple_edits = true,
+	default_file_explorer = true,
+	delete_to_trash = true,
+	columns = {
+		"icon",
+	},
+	view_options = {
+		show_hidden = true,
+	},
+})
