fe3910821b77badf101669829d326b0a895d37b4
Kiarash Kiani <kiarash.kiani@icloud.com>
2024-01-13 15:08:27 +0100

    feat(fish): add edit command for pkg

--- fish/functions/pkg.fish
+++ fish/functions/pkg.fish
@@ -1,11 +1,13 @@
 function pkg --description "Synchronize Homebrew packages"
-    argparse 'a/all' 'i/install-only' 'u/uninstall-only' -- $argv
-    if set -ql _flag_all 
-        brew bundle --force cleanup --file ~/.dotfiles/Brewfile 
+    argparse 'a/all' 'i/install-only' 'u/uninstall-only' 'e/edit-brewfile' -- $argv
+    if set -ql _flag_all
+        brew bundle --force cleanup --file ~/.dotfiles/Brewfile
         brew bundle --file ~/.dotfiles/Brewfile
     else if set -ql _flag_install_only
         brew bundle --file ~/.dotfiles/Brewfile
     else if set -ql _flag_uninstall_only
-        brew bundle --force cleanup --file ~/.dotfiles/Brewfile 
+        brew bundle --force cleanup --file ~/.dotfiles/Brewfile
+    else if set -ql _flag_edit_brewfile
+        nvim ~/.dotfiles/Brewfile
     end
 end
