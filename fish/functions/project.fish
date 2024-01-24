ed0125bba0c9bf097b57afe4584be9d6abf07b20
Kiarash Kiani <kiarash.kiani@icloud.com>
2024-01-24 12:16:41 +0100

    feat(fish): improve project command ui

--- fish/functions/project.fish
+++ fish/functions/project.fish
@@ -1,4 +1,4 @@
 function project --description "Open a project from the projects directory"
-    set -l prj_dir $(find ~/projects -maxdepth 2 -type d -mindepth 2 | fzf)
+    set -l prj_dir $(find ~/projects -maxdepth 2 -type d -mindepth 2 | fzf --border=rounded --color='border:green' --border-label="Jumping into a project")
     z $prj_dir
 end
