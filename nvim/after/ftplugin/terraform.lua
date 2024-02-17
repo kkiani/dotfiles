50d563079e0cc6595fbc1e6faad68da1d6fe85a1
Kiarash Kiani <kiarash.kiani@icloud.com>
2024-02-17 23:24:17 +0100

    feat(nvim): add terraform commands

--- nvim/after/ftplugin/terraform.lua
+++ nvim/after/ftplugin/terraform.lua
@@ -1,3 +1,18 @@
 vim.opt.colorcolumn = "120"
 vim.opt.shiftwidth = 2
 vim.opt.tabstop = 2
+
+-- Terraform commands
+vim.api.nvim_create_user_command("ValidateTerraform", function()
+	vim.cmd("wa")
+	vim.cmd("term! terraform init -backend=false; terraform validate")
+	vim.cmd("setlocal nonumber norelativenumber")
+	vim.cmd("setlocal signcolumn=no")
+end, { desc = "Validate terraform codes" })
+
+vim.api.nvim_create_user_command("PlanTerraform", function()
+	vim.cmd("wa")
+	vim.cmd("term! terraform init; terraform plan")
+	vim.cmd("setlocal nonumber norelativenumber")
+	vim.cmd("setlocal signcolumn=no")
+end, { desc = "Validate terraform codes" })
