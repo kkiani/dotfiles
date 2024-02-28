aac6143a1634d63d25d6255c1aeb5d2a2f9710af
Kiarash Kiani <kiarash.kiani@icloud.com>
2024-02-28 08:14:40 +0100

    feat(fish): make project command have preview of the folder

--- fish/functions/project.fish
+++ fish/functions/project.fish
@@ -1,4 +1,11 @@
 function project --description "Open a project from the projects directory"
-    set -l prj_dir $(find ~/projects -maxdepth 2 -type d -mindepth 2 | fzf --border=rounded --color='border:green' --border-label="Jumping into a project")
-    z $prj_dir
+    set -l selected_line "echo {}"
+    set -l prj_files_preview "$selected_line | xargs exa -l -s type --icons"
+
+    z $(find ~/projects -maxdepth 2 -type d -mindepth 2 | \
+        fzf --reverse --no-multi --ansi --border=rounded \
+            --color='border:green' \
+            --border-label="Jumping into a project"\
+            --preview="$prj_files_preview" \
+    )
 end
