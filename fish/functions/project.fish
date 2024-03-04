17ddc5a4a663192d400c3ea43db360e846de92d1
Kiarash Kiani <kiarash.kiani@icloud.com>
2024-03-04 21:40:28 +0100

    feat(fish): add option to project command to go into sub directories

--- fish/functions/project.fish
+++ fish/functions/project.fish
@@ -1,11 +1,13 @@
 function project --description "Open a project from the projects directory"
     set -l selected_line "echo {}"
     set -l prj_files_preview "$selected_line | xargs exa -l -s type --icons"
+    set -l go_in_dir "$selected_line | xargs -I {} find {} -maxdepth 1 -type d -mindepth 1"
 
     z $(find ~/projects -maxdepth 2 -type d -mindepth 2 | \
         fzf --reverse --no-multi --ansi --border=rounded \
             --color='border:green' \
             --border-label="Jumping into a project"\
             --preview="$prj_files_preview" \
+            --bind "right:reload($go_in_dir)" \
     )
 end
