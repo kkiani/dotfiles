5187ce78f74d0af2c80c2a39a690662b85e7d56f
Kiarash Kiani <kiarash.kiani@icloud.com>
2024-03-17 14:14:04 +0100

    feat(xplr): add plugins

--- xplr/init.lua
+++ xplr/init.lua
@@ -1,5 +1,8 @@
 version = "0.21.5"
 local xplr = xplr
+local home = os.getenv("HOME")
+local xpm_path = home .. "/.local/share/xplr/dtomvan/xpm.xplr"
+local xpm_url = "https://github.com/dtomvan/xpm.xplr"
 
 -- Define the function
 xplr.fn.custom.ask_name_and_greet = function(app)
@@ -17,3 +20,22 @@ xplr.config.modes.builtin.default.key_bindings.on_key.space = {
 		{ CallLua = "custom.ask_name_and_greet" },
 	},
 }
+
+-- plugins
+package.path = package.path .. ";" .. xpm_path .. "/?.lua;" .. xpm_path .. "/?/init.lua"
+os.execute(string.format("[ -e '%s' ] || git clone '%s' '%s'", xpm_path, xpm_url, xpm_path))
+require("xpm").setup({
+	plugins = {
+		-- Let xpm manage itself
+		"dtomvan/xpm.xplr",
+		"prncss-xyz/icons.xplr",
+		{
+			"dtomvan/extra-icons.xplr",
+			after = function()
+				xplr.config.general.table.row.cols[2] = { format = "custom.icons_dtomvan_col_1" }
+			end,
+		},
+	},
+	auto_install = true,
+	auto_cleanup = true,
+})
