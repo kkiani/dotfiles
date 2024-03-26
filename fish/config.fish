877b9bc14a2ad24325815e39fab664f678dead0c
Kiarash Kiani <kiarash.kiani@icloud.com>
2024-03-26 23:16:59 +0100

    fix(fish): move env variables to a seprate file

--- fish/config.fish
+++ fish/config.fish
@@ -8,6 +8,7 @@ fish_vi_key_bindings
 source ~/.config/fish/alias.fish
 source ~/.config/fish/abbr.fish
 source ~/.config/fish/key_bindings.fish
+source ~/.config/fish/environment.fish
 source /usr/local/opt/asdf/libexec/asdf.fish
 direnv hook fish | source
 zoxide init fish | source
