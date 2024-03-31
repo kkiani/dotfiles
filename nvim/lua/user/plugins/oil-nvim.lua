54cf1dddc32ee4f4275ea9e72d62186070d096bb
Kiarash Kiani <kiarash.kiani@icloud.com>
2024-03-31 15:50:13 +0200

    fix: move oil plugin config to lazy

--- nvim/lua/user/plugins/oil-nvim.lua
+++ nvim/lua/user/plugins/oil-nvim.lua
@@ -1,3 +1,16 @@
 return {
-"stevearc/oil.nvim"
+	"stevearc/oil.nvim",
+	config = function()
+		require("oil").setup({
+			skip_confirm_for_simple_edits = true,
+			default_file_explorer = true,
+			delete_to_trash = true,
+			columns = {
+				"icon",
+			},
+			view_options = {
+				show_hidden = true,
+			},
+		})
+	end,
 }
