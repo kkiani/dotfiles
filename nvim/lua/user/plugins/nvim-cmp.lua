e1d76480a35f25a0696f5ced47af4363316ac9e0
Kiarash Kiani <kiarash.kiani@icloud.com>
2024-03-31 16:05:49 +0200

    fix: lsp-zero and nvim-cmp configs moved to lazy

--- nvim/lua/user/plugins/nvim-cmp.lua
+++ nvim/lua/user/plugins/nvim-cmp.lua
@@ -1,3 +1,9 @@
 return {
-"hrsh7th/nvim-cmp"
+	"hrsh7th/nvim-cmp",
+	dependencies = {
+		{ "hrsh7th/cmp-nvim-lsp" },
+		{ "hrsh7th/cmp-buffer" },
+		{ "hrsh7th/cmp-path" },
+		{ "hrsh7th/cmp-cmdline" },
+	},
 }
