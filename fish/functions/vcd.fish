a946c180871ae39513615f836b878ac85f72e593
Kiarash Kiani <kiarash.kiani@icloud.com>
2024-04-02 15:09:22 +0200

    feat(fish): remove old unused function

--- fish/functions/vcd.fish
+++
@@ -1,8 +0,0 @@
-function vcd --description 'Change directory using vifm'
-    set dst "$(command vifm --choose-dir - $argv[2..-1])"
-    if [ -z "$dst" ];
-        echo 'Directory picking cancelled/failed'
-        return 1
-    end
-    z "$dst"
-end
