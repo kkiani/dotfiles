e144f560a5739d4755d7389aed11814f470602e6
Kiarash Kiani <kiarash.kiani@icloud.com>
2024-04-17 21:37:30 +0200

    fix(nvim): indent lines color

--- nvim/lua/user/plugins/indent-blankline-nvim.lua
+++ nvim/lua/user/plugins/indent-blankline-nvim.lua
@@ -1,13 +1,14 @@
 return {
 	"lukas-reineke/indent-blankline.nvim",
+	dependencies = { "ellisonleao/gruvbox.nvim" },
 	config = function()
 		require("ibl").setup({
 			indent = {
-				highlight = "CursorLineSign",
+				highlight = "GruvboxBg1",
 				char = "▏",
 			},
 			scope = {
-				highlight = "CursorLineFold",
+				highlight = "GruvboxBg4",
 				char = "▏",
 				include = {
 					node_type = {
