e5b29c28972d03affbd3ed89cf9fb184ec6f517a
Kiarash Kiani <kiarash.kiani@icloud.com>
2024-04-25 11:29:55 +0200

    feat(fish): update the preview position of the project command

--- fish/functions/project.fish
+++ fish/functions/project.fish
@@ -1,6 +1,6 @@
 function project --description "Open a project from the projects directory"
     set -l selected_line "echo {}"
-    set -l prj_files_preview "$selected_line | xargs exa -l -s type --icons"
+    set -l prj_files_preview "$selected_line | xargs exa -s type --icons"
     set -l go_in_dir "$selected_line | xargs -I {} find {} -maxdepth 1 -type d -mindepth 1"
 
     z $(find ~/projects -maxdepth 2 -type d -mindepth 2 | \
@@ -8,6 +8,7 @@ function project --description "Open a project from the projects directory"
             --color='border:green' \
             --border-label="Jumping into a project"\
             --preview="$prj_files_preview" \
+            --preview-window=down:20% \
             --bind "right:reload($go_in_dir)" \
     )
 end
