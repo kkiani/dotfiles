58917489d1f0a2e0859aefc440c0910af879c0a3
Kiarash Kiani <kiarash.kiani@icloud.com>
2024-04-26 14:36:28 +0200

    fix(nvim): add all types of semantic commits

--- nvim/lua/user/snippets/gitcommit.lua
+++ nvim/lua/user/snippets/gitcommit.lua
@@ -1,32 +1,62 @@
 local ls = require("luasnip")
+local fmt = require("luasnip.extras.fmt").fmt
 
-local snippet = ls.snippet
-local text = ls.text_node
-local insert = ls.insert_node
+local s = ls.snippet
+local i = ls.insert_node
 
 ls.add_snippets("gitcommit", {
-	snippet({ trig = "feat", dscr = "a new feature added", snippetType = "autosnippet" }, {
-		text("feat("),
-		insert(1),
-		text("): "),
-		insert(2),
-	}),
+	s(
+		{ trig = "feat", dscr = "New feature commit with scope", snippetType = "autosnippet" },
+		fmt("feat({}): {}", {
+			i(1, "scope"),
+			i(2, "description"),
+		})
+	),
 })
 
 ls.add_snippets("gitcommit", {
-	snippet({ trig = "fix", dscr = "fixing a bug", snippetType = "autosnippet" }, {
-		text("fix("),
-		insert(1),
-		text("): "),
-		insert(2),
-	}),
+	s(
+		{ trig = "feat", dscr = "New feature commit without scope", snippetType = "autosnippet" },
+		fmt("feat: {}", {
+			i(1, "description"),
+		})
+	),
 })
 
 ls.add_snippets("gitcommit", {
-	snippet({ trig = "chore", dscr = "a chore change", snippetType = "autosnippet" }, {
-		text("chore("),
-		insert(1),
-		text("): "),
-		insert(2),
-	}),
+	s(
+		{ trig = "fix", dscr = "New fix commit with scope", snippetType = "autosnippet" },
+		fmt("fix({}): {}", {
+			i(1, "scope"),
+			i(2, "description"),
+		})
+	),
+})
+
+ls.add_snippets("gitcommit", {
+	s(
+		{ trig = "fix", dscr = "New fix commit without scope", snippetType = "autosnippet" },
+		fmt("fix: {}", {
+			i(1, "description"),
+		})
+	),
+})
+
+ls.add_snippets("gitcommit", {
+	s(
+		{ trig = "chore", dscr = "New chore commit with scope", snippetType = "autosnippet" },
+		fmt("chore({}): {}", {
+			i(1, "scope"),
+			i(2, "description"),
+		})
+	),
+})
+
+ls.add_snippets("gitcommit", {
+	s(
+		{ trig = "chore", dscr = "New chore commit without scope", snippetType = "autosnippet" },
+		fmt("chore: {}", {
+			i(1, "description"),
+		})
+	),
 })
