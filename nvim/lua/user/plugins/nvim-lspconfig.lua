9e5c33c9299680fdfd3ea5f1cec7f37096480e74
Kiarash Kiani <kiarash.kiani@icloud.com>
2024-04-26 12:01:55 +0200

    fix(nvim): make nvim variables for lua lsp

--- nvim/lua/user/plugins/nvim-lspconfig.lua
+++ nvim/lua/user/plugins/nvim-lspconfig.lua
@@ -13,11 +13,32 @@ return {
 
 		local lspconfig = require("lspconfig")
 
-		-- lspconfig.lua_ls.setup(lsp.nvim_lua_ls())
-		lspconfig.lua_ls.setup({})
 		lspconfig.tsserver.setup({})
 		lspconfig.rust_analyzer.setup({})
 		lspconfig.pyright.setup({})
 		lspconfig.terraform_lsp.setup({})
+		lspconfig.lua_ls.setup({
+			on_init = function(client)
+				local path = client.workspace_folders[1].name
+				if vim.loop.fs_stat(path .. "/.luarc.json") or vim.loop.fs_stat(path .. "/.luarc.jsonc") then
+					return
+				end
+
+				client.config.settings.Lua = vim.tbl_deep_extend("force", client.config.settings.Lua, {
+					runtime = {
+						version = "LuaJIT",
+					},
+					workspace = {
+						checkThirdParty = false,
+						library = {
+							vim.env.VIMRUNTIME,
+						},
+					},
+				})
+			end,
+			settings = {
+				Lua = {},
+			},
+		})
 	end,
 }
