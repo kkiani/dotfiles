6ca481d07e8648f56ab2241fbfc1793e49af12ab
Kiarash Kiani <kiarash.kiani@icloud.com>
2024-04-26 14:48:31 +0200

    feat(term): remove ui customizations

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -6,24 +6,12 @@ config.font = wezterm.font("JetBrainsMonoNL Nerd Font")
 config.font_size = 14.0
 config.command_palette_font_size = 22.0
 config.enable_tab_bar = false
-config.window_decorations = "RESIZE"
 config.window_close_confirmation = "NeverPrompt"
 config.window_padding = {
 	left = 0,
 	right = 0,
 	top = 0,
 	bottom = 0,
-}
-
-config.window_frame = {
-	border_left_width = "1.0cell",
-	border_right_width = "1.0cell",
-	border_bottom_height = "0.5cell",
-	border_top_height = "0.5cell",
-	border_left_color = "gray",
-	border_right_color = "gray",
-	border_bottom_color = "gray",
-	border_top_color = "gray",
 }
 
 config.keys = {
