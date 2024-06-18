e4b91e968286b02c59793f70ab2381c35e35b946
Kiarash Kiani <kiarash.kiani@icloud.com>
2024-06-18 10:04:45 +0200

    fix(nvim): update the oil config

--- nvim/lua/user/plugins/oil-nvim.lua
+++ nvim/lua/user/plugins/oil-nvim.lua
@@ -1,15 +1,20 @@
 return {
 	"stevearc/oil.nvim",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
 	config = function()
 		require("oil").setup({
-			skip_confirm_for_simple_edits = true,
 			default_file_explorer = true,
 			delete_to_trash = true,
-			columns = {
-				"icon",
-			},
+			skip_confirm_for_simple_edits = true,
 			view_options = {
 				show_hidden = true,
+				natural_order = true,
+				is_always_hidden = function(name, _)
+					return name == ".." or name == ".git"
+				end,
+			},
+			columns = {
+				"icon",
 			},
 		})
 	end,
