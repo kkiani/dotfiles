5588409d3fa6c3ffb5021f5104d1c3c16b22d030
Kiarash Kiani <kiarash.kiani@icloud.com>
2024-07-08 11:45:14 +0200

    feat(fish): add new command for easy browsing git diff files

---
+++ fish/functions/diffbrowse.fish
@@ -0,0 +1,13 @@
+function diffbrowse --description "Browse diff of files against a branch"
+    set -l branch $argv[1]
+    set -l selected "echo {}"
+    set -l previewer "$selected | git diff master -- {} | delta -w $COLUMNS"
+
+
+    git diff --name-only master | \
+    fzf --reverse --no-multi --ansi \
+        --color='border:green' \
+        --border-label="Jumping into a project"\
+        --preview="$previewer" \
+        --preview-window=up:80%
+end
