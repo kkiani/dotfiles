b66e12a16197dda78b81af9655fe0fd6ac60cebb
Kiarash Kiani <kiarash.kiani@icloud.com>
2024-07-09 09:05:13 +0200

    fix(nvim): telescope randomly breaks when opening nvim

--- nvim/lua/user/plugins/oil-nvim.lua
+++ nvim/lua/user/plugins/oil-nvim.lua
@@ -1,6 +1,7 @@
 return {
 	"stevearc/oil.nvim",
 	dependencies = { "nvim-tree/nvim-web-devicons" },
+	lazy = false,
 	config = function()
 		require("oil").setup({
 			default_file_explorer = true,
--- nvim/lua/user/plugins/telescope-nvim.lua
+++ nvim/lua/user/plugins/telescope-nvim.lua
@@ -1,6 +1,7 @@
 return {
 	"nvim-telescope/telescope.nvim",
 	tag = "0.1.8",
+    lazy = false,
 	dependencies = { { "nvim-lua/plenary.nvim" } },
 	config = function()
 		require("telescope").setup({
