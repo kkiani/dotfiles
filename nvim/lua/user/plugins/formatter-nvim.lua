53a5b373610b84cf872b5ee9c660bc47aec6d65f
Kiarash Kiani <kiarash.kiani@icloud.com>
2024-07-15 11:59:58 +0200

    fix(nvim): nvim was broken on launch randomly.

--- nvim/lua/user/plugins/formatter-nvim.lua
+++ nvim/lua/user/plugins/formatter-nvim.lua
@@ -1,40 +1,38 @@
-local util = require("formatter.util")
-
-local function prettier()
-	return {
-		exe = "prettier",
-		args = {
-			util.escape_path(util.get_current_buffer_file_path()),
-		},
-		stdin = true,
-	}
-end
-
 return {
 	"mhartington/formatter.nvim",
-	opts = {
-		-- Enable or disable logging
-		logging = true,
-		-- Set the log level
-		log_level = vim.log.levels.WARN,
-		-- All formatter configurations are opt-in
-		filetype = {
-			-- Formatter configurations for filetype "lua" go here
-			-- and will be executed in order
-			lua = { require("formatter.filetypes.lua").stylua },
-			python = { require("formatter.filetypes.python").black },
-			typescript = { prettier },
-			javascript = { prettier },
-			json = { prettier },
-			yaml = { prettier },
-			terraform = { require("formatter.filetypes.terraform").terraformfmt },
-			rust = { require("formatter.filetypes.rust").rustfmt },
+	lazy = false,
+	config = function()
+		local util = require("formatter.util")
 
-			-- Use the special "*" filetype for defining formatter configurations on
-			-- any filetype
-			["*"] = {
-				require("formatter.filetypes.any").remove_trailing_whitespace,
+		local function prettier()
+			return {
+				exe = "prettier",
+				args = {
+					util.escape_path(util.get_current_buffer_file_path()),
+				},
+				stdin = true,
+			}
+		end
+
+		require("formatter").setup({
+			filetype = {
+				-- Formatter configurations for filetype "lua" go here
+				-- and will be executed in order
+				lua = { require("formatter.filetypes.lua").stylua },
+				python = { require("formatter.filetypes.python").black },
+				typescript = { prettier },
+				javascript = { prettier },
+				json = { prettier },
+				yaml = { prettier },
+				terraform = { require("formatter.filetypes.terraform").terraformfmt },
+				rust = { require("formatter.filetypes.rust").rustfmt },
+
+				-- Use the special "*" filetype for defining formatter configurations on
+				-- any filetype
+				["*"] = {
+					require("formatter.filetypes.any").remove_trailing_whitespace,
+				},
 			},
-		},
-	},
+		})
+	end,
 }
--- nvim/lua/user/plugins/telescope-nvim.lua
+++ nvim/lua/user/plugins/telescope-nvim.lua
@@ -1,8 +1,8 @@
 return {
 	"nvim-telescope/telescope.nvim",
 	tag = "0.1.8",
-    lazy = false,
-	dependencies = { { "nvim-lua/plenary.nvim" } },
+	lazy = false,
+	dependencies = { "nvim-lua/plenary.nvim" },
 	config = function()
 		require("telescope").setup({
 			defaults = {
