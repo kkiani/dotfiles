0f745ff62140ff26c1401441912dcd8dfcda5784
Kiarash Kiani <kiarash.kiani@icloud.com>
2024-08-02 08:47:01 +0200

    feat(wezterm): add smart toggle pane

--- wezterm/keymaps.lua
+++ wezterm/keymaps.lua
@@ -55,4 +55,25 @@ return {
 		mods = "CMD|SHIFT",
 		action = wezterm.action.ActivatePaneDirection("Left"),
 	},
+	{
+		key = "s",
+		mods = "CMD|SHIFT",
+		action = wezterm.action_callback(function(_, pane)
+			local tab = pane:tab()
+			local panes = tab:panes_with_info()
+			if #panes == 1 then
+				if pane:get_dimensions().pixel_width > pane:get_dimensions().pixel_height then
+					pane:split({ direction = "Right" })
+				else
+					pane:split({ direction = "Bottom" })
+				end
+			elseif not panes[1].is_zoomed then
+				panes[1].pane:activate()
+				tab:set_zoomed(true)
+			elseif panes[1].is_zoomed then
+				tab:set_zoomed(false)
+				panes[2].pane:activate()
+			end
+		end),
+	},
 }
