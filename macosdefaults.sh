6c75b47f9cb28f07e0acb0ffd8f6a115d0c5a7ef
Kiarash Kiani <kiarash.kiani@icloud.com>
2024-11-08 14:03:39 +0100

    feat: update the macos defaults configs

--- macosdefaults.sh
+++ macosdefaults.sh
@@ -5,6 +5,8 @@
 # The original idea (and a couple settings) were grabbed from:
 #   https://github.com/mathiasbynens/dotfiles/blob/master/.macos
 
+defaults write -g AppleShowScrollBars WhenScrolling
+
 # Disable press-and-hold for keys in favor of key repeat.
 defaults write -g ApplePressAndHoldEnabled -bool false
 
