683cf2af68c0caccf3b6cc5a64ecfd7cf6370e09
Kiarash Kiani <kiarash.kiani@icloud.com>
2024-12-26 18:55:25 +0100

    fix: using brew prefix for packages that are install using brew

--- fish/config.fish
+++ fish/config.fish
@@ -1,18 +1,19 @@
+# homebrew
+eval "$(/opt/homebrew/bin/brew shellenv)"
+set -U BREW_PREFIX "$(brew --prefix)"
+
 if status is-interactive
     # starship
-    source (/opt/homebrew/bin/starship init fish --print-full-init | psub)
+    source ($BREW_PREFIX/bin/starship init fish --print-full-init | psub)
 end
 
 fish_vi_key_bindings
-
-# homebrew
-eval "$(/opt/homebrew/bin/brew shellenv)"
 
 source ~/.config/fish/alias.fish
 source ~/.config/fish/abbr.fish
 source ~/.config/fish/key_bindings.fish
 source ~/.config/fish/environment.fish
-source /usr/local/opt/asdf/libexec/asdf.fish
+source $BREW_PREFIX/opt/asdf/libexec/asdf.fish
 direnv hook fish | source
 zoxide init fish | source
 atuin init fish | source
