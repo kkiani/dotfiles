21f442471d320e87387fafdb2bb3569faaf4bb2d
Kiarash Kiani <kiarash.kiani@icloud.com>
2025-01-03 20:52:22 +0100

    feat: keybinding for important shortcuts

--- fish/key_bindings.fish
+++ fish/key_bindings.fish
@@ -1,3 +1,10 @@
 bind -M insert \cU 'cd ..; commandline -f repaint'
-bind -M insert \cO 'yazi'
-bind \cO 'yazi'
+
+bind -M insert \cO 'y; commandline -f repaint'
+bind \cO 'y; commandline -f repaint'
+
+bind -M insert \cD 'y ~/Downloads; commandline -f repaint'
+bind \cD 'y ~/Downloads; commandline -f repaint'
+
+bind -M insert \cP 'y ~/projects; commandline -f repaint'
+bind \cP 'y ~/projects; commandline -f repaint'
