9d9cca7b2fc4a7223ca6a93b98035ad5752c2005
Kiarash Kiani <kiarash.kiani@icloud.com>
2025-03-24 08:54:07 +0100

    fix: asdf not working

--- fish/config.fish
+++ fish/config.fish
@@ -16,7 +16,20 @@ source ~/.config/fish/alias.fish
 source ~/.config/fish/abbr.fish
 source ~/.config/fish/key_bindings.fish
 source ~/.config/fish/environment.fish
-source $BREW_PREFIX/opt/asdf/libexec/asdf.fish
 direnv hook fish | source
 zoxide init fish | source
 atuin init fish | source
+
+# ASDF configuration code
+if test -z $ASDF_DATA_DIR
+    set _asdf_shims "$HOME/.asdf/shims"
+else
+    set _asdf_shims "$ASDF_DATA_DIR/shims"
+end
+
+# Do not use fish_add_path (added in Fish 3.2) because it
+# potentially changes the order of items in PATH
+if not contains $_asdf_shims $PATH
+    set -gx --prepend PATH $_asdf_shims
+end
+set --erase _asdf_shims
