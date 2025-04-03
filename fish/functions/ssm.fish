37c1c80eed2bb2802ac82d8f8c82ea50e08d3276
Kiarash Kiani <kiarash.kiani@icloud.com>
2025-04-03 07:54:11 +0200

    fix(fish): shell become unusable after exiting from ssm command

--- fish/functions/ssm.fish
+++ fish/functions/ssm.fish
@@ -1,4 +1,28 @@
-set -l instance_name $(aws ec2 describe-instances | jq '.Reservations' | jq -r '.[].Instances | .[0].Tags | .[] | select(.Key == "Name") | .Value' | fzf)
-set -l instance_id $(aws ec2 describe-instances --filters 'Name=tag:Name,Values='$instance_name | jq -r '.Reservations | .[0].Instances | .[0].InstanceId')
+function ssm --description "Attach to an available EC2 instance"
 
-aws ssm start-session --target $instance_id
+    set -l instance_name $(aws ec2 describe-instances | jq '.Reservations' | jq -r '.[].Instances | .[0].Tags | .[] | select(.Key == "Name") | .Value' | fzf)
+
+    if test -z "$instance_name"
+        echo "No instance selected. Aborting."
+        return 1 # Indicate failure
+    end
+
+    set -l instance_id $(aws ec2 describe-instances --filters 'Name=tag:Name,Values='$instance_name | jq -r '.Reservations | .[0].Instances | .[0].InstanceId')
+
+    echo "Selected instance: $instance_name"
+
+    if test -z "$instance_id"
+        echo "Error: Could not find Instance ID for '$instance_name' or it's not running. Aborting."
+        aws ec2 describe-instances --filters "Name=tag:Name,Values=$instance_name" # Show details for debugging
+        return 1 # Indicate failure
+    end
+
+    echo "Found Instance ID: $instance_id"
+    echo "Starting SSM session..."
+
+    aws ssm start-session --target $instance_id
+
+    set -l last_status $status
+    return $last_status
+
+end
